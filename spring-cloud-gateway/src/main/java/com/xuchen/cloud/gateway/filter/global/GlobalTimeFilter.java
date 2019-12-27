c8950ef6e470b48e59d5cbc98fef335346f0fd72
edwin.wang <edwin.wang@movit-tech.com>
2019-12-27 14:52:28 +0800

    从eureka切换至consul

--- spring-cloud-gateway/src/main/java/com/xuchen/cloud/gateway/filter/global/GlobalTimeFilter.java
+++ spring-cloud-gateway/src/main/java/com/xuchen/cloud/gateway/filter/global/GlobalTimeFilter.java
@@ -1,12 +1,23 @@
 package com.xuchen.cloud.gateway.filter.global;
 
+import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.security.Principal;
+import java.util.Map;
 
 /**
  * @author xuchen
@@ -14,12 +25,16 @@ import reactor.core.publisher.Mono;
  * 全局过滤器
  */
 @Slf4j
-//@Component
+@Component
 public class GlobalTimeFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("[全局拦截]进入耗时拦截器");
+        log.info(JSONUtil.toJsonStr(exchange.getAttributes()));
+        Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+        log.info(route.getUri().toString());
+        log.info(route.toString());
         exchange.getAttributes().put("ELAPSED_TIME_BEGIN", System.currentTimeMillis());
         return chain.filter(exchange).then(
                 Mono.fromRunnable(() -> {
